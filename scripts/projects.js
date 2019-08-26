d3f8d8e4e2c979ac375597e36be1b197270f12d9
Michał Białecki <miszekb@gmail.com>
2019-08-26 22:39:52 +0200

    [EDIT] big portfolio rebuild

---
+++ scripts/projects.js
@@ -0,0 +1,19 @@
+githubButtonHandler = () => {
+    window.open('https://github.com/miszekb', '_blank');
+}
+
+facebookButtonHandler = () => {
+   // window.open('https://www.codexworld.com', '_blank');
+}
+
+linkedinButtonHandler = () => {
+    window.open('https://www.linkedin.com/in/miszekb/', '_blank');
+}
+
+fakeLoadingAnim = () => {
+
+}
+
+downloadResumeHandler = () => {
+    window.open('../files/CV.pdf', '_blank');
+}
